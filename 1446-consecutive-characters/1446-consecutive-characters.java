2aa6cc5707177577253109f1912aa138aa64aae0
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-02-18 13:47:18 +0530

    Time: 1 ms (100%), Space: 43.5 MB (63.62%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxPower(String s) {
+        int max=1;  int count=1;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+            }
+            else{
+                count=1;
+            }
+            max=Math.max(count,max);
+        }
+        return max;
+        
+    }
+}
