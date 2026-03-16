c78c4a3840daecad124766ed21fdd0f5c94620ce
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-03-16 09:51:28 +0530

    Time: 4 ms (99.93%), Space: 51.4 MB (32.38%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,23 +1,18 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        
+        //sliding window single variation
+        int prod=1;
         int count=0;
-        
-       
+        int start=0;
         for(int i=0; i<nums.length; i++){
-            int prod=1;
+            prod=prod*nums[i];
+            while(prod>=k &&start<=i){
+                prod/=nums[start++];
 
-            for(int j=i; j<nums.length; j++){
-                prod =prod*nums[j];
-                if(prod>=k)break;
-                    
-                    count++;
-                    
-                
-                
+
             }
+            count+=(i-start+1);
         }
         return count;
-        
     }
 }
