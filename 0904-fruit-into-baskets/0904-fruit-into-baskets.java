a48d257d11e56884bebd372c106657a1f3e08db1
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-03-16 17:06:50 +0530

    Time: 54 ms (65.64%), Space: 70.7 MB (38.36%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,30 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-       int []fr=new int[100001];
-       int type=0;
-       int max=0;
-       int s=0;
-      // int last=0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int s = 0;
+        int max = 0;
 
-       for(int e=0; e<fruits.length; e++){
-        int f=fruits[e];
-        if(fr[f]==0)type++;
-        fr[f]++;
-        while(type>2){
-            int sf=fruits[s];
-            fr[sf]--;
-            if(fr[sf]==0)type--;
-            s++;
+        for(int e = 0; e < fruits.length; e++) {
 
+            // add fruit in window
+            map.put(fruits[e], map.getOrDefault(fruits[e], 0) + 1);
+
+            // shrink window if types > 2
+            while(map.size() > 2) {
+                map.put(fruits[s], map.get(fruits[s]) - 1);
+
+                if(map.get(fruits[s]) == 0) {
+                    map.remove(fruits[s]);
+                }
+                s++;
+            }
+
+            // update answer
+            max = Math.max(max, e - s + 1);
         }
-        int pick=e-s+1;
-        max=Math.max(max,pick);
 
-        
-
-        
-       }
-       return max;
-
+        return max;
     }
 }
